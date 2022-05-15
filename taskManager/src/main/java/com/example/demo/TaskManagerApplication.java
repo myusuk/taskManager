506fa9927261ec9d36cbd38bc8179f73841eb395
myusuk <mashiko1939@gmail.com>
2022-05-15 15:55:18 +0900

    Connect Thymeleaf Layout

--- taskManager/src/main/java/com/example/demo/TaskManagerApplication.java
+++ taskManager/src/main/java/com/example/demo/TaskManagerApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 
 @SpringBootApplication
 public class TaskManagerApplication {
-	
 	
 
 	public static void main(String[] args) {
