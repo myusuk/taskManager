8a8963ca138467a6289b346e09d82a96c1f18fa4
myusuk <mashiko1939@gmail.com>
2022-06-17 21:32:42 +0900

    #0005 システム個別ページの作成

--- taskManager/src/main/resources/static/js/task-complate.js
+++ taskManager/src/main/resources/static/js/task-complate.js
@@ -11,10 +11,6 @@
 			})
 	};
 	
-	//モーダル内容をリセット
-	function modalReset(){
-	}
-	
 	//モーダルにデータをセット
 	function setData(data){
 		$('#taskId').val(data.id);
@@ -27,28 +23,11 @@
 		$('#startDateText').text(data.startDate);
 	}
 	
-	//モーダルを入力不可にする
-	
-	
 	$('#task-modal').on('show.bs.modal', function (event) {
-		
 		var button = $(event.relatedTarget);
-		var type = button.context.dataset.type;
 		var id = button.data('id');
-		
-		modalReset();
-		
-		switch(type){
-			case 'edit':
-			getone(id);
-			document.getElementById("task-form").setAttribute("action", "/task/edit");
-			$('#modal-title').text('タスク編集');
-			break;
-			case 'delete':
-			getone(id);
-			document.getElementById("task-form").setAttribute("action", "/task/delete");
-			$('#modal-title').text('タスク削除');
-			break;
-		}
+		getone(id);
+		document.getElementById("task-form").setAttribute("action", "/task/edit");
+		$('#modal-title').text('タスク編集');
 	})
 	
